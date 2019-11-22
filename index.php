5f702ae4914ee06b1213505aac32dca3c7798bbc
Jose Luis <jluisv987@gmail.com>
2019-11-22 01:17:47 -0600

    Pag ind

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php include('includes/database.php');?>
 <?php include('includes/header.php');?>
 <?php
-    $query="SELECT libros.titulo AS 'titulo',
+    $query="SELECT libros.id_libro AS 'id_libro',
+    libros.titulo AS 'titulo',
     libros.ISBN13 AS 'ISBN13',
     autores.nombre_autor AS 'nombre_autor',
     autores.apellido_autor AS 'apellido_autor',
     libros.portada AS 'portada'
     FROM libros,autores,libros_autores
-    WHERE libros.id_libro = libros_autores.libros_id_libro
+    WHERE id_libro = libros_autores.libros_id_libro
     AND autores.id_autor=libros_autores.autores_id_autor";
     //Consigue los titulos
     $result= $mysqli->query($query) or die($mysqli->error.__LINE__);
@@ -57,7 +58,7 @@
                                 if($aux!=$row['titulo'])
                                 {
                                     echo "<tr>";
-                                    echo "<td>" . $row['titulo'] . "</td>";
+                                    echo "<td><a href='libro.php?id=".$row['id_libro']."'>" . $row['titulo'] ."</a></td>";
                                     echo "<td>" . $row['ISBN13'] . "</td>";
                                     echo "<td>".$row['nombre_autor']." ".$row['apellido_autor']."</td>";
                                     echo "<td>".'<img src="data:image;base64,'.base64_encode($row['portada']).'"alt="Image" style="width:100px;height:150px;"'."</td>";
